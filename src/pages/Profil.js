055040e65ece96b240c041c6b78dba600a181c5c
benaor <benjamingirard25@gmail.com>
2021-07-19 13:56:03 +0200

    setting up the router

---
+++ src/pages/Profil.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profil = () => {
+    return ( 
+        <div>
+            Profil page
+        </div>
+     );
+}
+ 
+export default Profil;
---
+++ src/pages/Trending.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Trending = () => {
+    return ( 
+        <div>
+            Page trending
+        </div>
+     );
+}
+ 
+export default Trending;
